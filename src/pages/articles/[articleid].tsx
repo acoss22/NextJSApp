5116ecf27b131027cf46f1936587e4b01020df84
acoss22 <anacseq@gmail.com>
2023-04-16 23:54:40 +0100

    log test

--- src/pages/articles/[articleid].tsx
+++ src/pages/articles/[articleid].tsx
@@ -3,8 +3,9 @@ import { useRouter } from "next/router";
 function ArticlePage() {
   const router = useRouter();
 
-  console.log(router.pathname);
-  console.log(router.route);
+  console.log("router.pathname", router.pathname);
+  console.log("route", router.route);
+  console.log("query", router.query);
 
 
   return (
